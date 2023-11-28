fd7c9f925d21660fd2ad1e8df12e0d624876da93
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2023-11-28 16:02:27 +0000

    Fix LED.

--- build_debian.sh
+++ build_debian.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # https://www.taterli.com/9532/
 
+rm -f linux_5.10/build/cv1800b_milkv_duo_sd/.config
+
 . milkv/boardconfig-milkv-duo.sh
 . build/${MV_BUILD_ENV}
 defconfig ${MV_BOARD_LINK}
